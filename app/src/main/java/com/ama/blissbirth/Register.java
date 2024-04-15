dada720a4a4c4d5a60096347b3852cdf0d47d17a
marcosromera <marcosromlu@gmail.com>
2024-04-15 19:01:40 +0200

    Diseño del register

---
+++ app/src/main/java/com/ama/blissbirth/Register.java
@@ -0,0 +1,19 @@
+package com.ama.blissbirth;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+    }
+}
