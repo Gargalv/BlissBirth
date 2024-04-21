c36f3fca9e8b967401ea6093f55d7c1a48b75586
gargalv <albertogarciag.2003@gmail.com>
2024-04-21 12:58:13 +0200

    Register commit

--- app/src/main/java/com/ama/blissbirth/Register.java
+++ app/src/main/java/com/ama/blissbirth/Register.java
@@ -3,17 +3,27 @@ package com.ama.blissbirth;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
+    private Button su;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+        su = findViewById(R.id.register);
+        su.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRegister = new Intent(Register.this, Main.class);
+                startActivity(toRegister);
+            }
+        });
 
     }
 }
